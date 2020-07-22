cefa6703f4b11abaf80888d3a0ac5f2d38d20520
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-22 18:14:01 -0400

    build

---
+++ build/precache-manifest.6a6186846d6f971916fd618a41cfccc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1f7219bb950927ccbca9fbbf5b6e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7511683573f857e21629",
+    "url": "/static/css/main.4736a54d.chunk.css"
+  },
+  {
+    "revision": "6b804aa2c106971c646d",
+    "url": "/static/js/2.79a42a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79a42a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7511683573f857e21629",
+    "url": "/static/js/main.febbe6ab.chunk.js"
+  },
+  {
+    "revision": "31292e2cc0e36195b0c1",
+    "url": "/static/js/runtime-main.87e0fef7.js"
+  }
+]);
