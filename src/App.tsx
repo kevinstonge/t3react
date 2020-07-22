5f8076fa18f5d2cfdef78fe839abf1e5b3d72589
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-22 06:16:55 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Board from './Board';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {
+  
 }
-
+ 
+export interface appState {
+    "first":Boolean;
+    "second":Boolean;
+}
+ 
+class App extends React.Component<AppProps, appState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { 
+      "first":true,
+      "second":true
+    };
+  }
+  render() { 
+    return (
+      <div className="App">
+        <h1>Tic Tac Toe - ReactJS (--typescript)</h1>
+        <Board {this.state<appState>}/>
+      </div>
+    );
+  }
+}
+ 
 export default App;
---
+++ src/Board.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export interface appState {
+    "first":Boolean;
+    "second":Boolean;
+}
+ 
+export interface BoardState {
+    "boardMap":Boolean[]
+}
+ 
+class Board extends React.Component<appState, BoardState> {
+    constructor(props: appState) {
+        super(props);
+        this.state = { "boardMap":new Array(9).fill(false) };
+    }
+    render() { 
+        return (
+            <div>
+                <p>{this.state["boardMap"]}</p>
+                <p>{JSON.stringify(this.state)}</p>
+                <p>{JSON.stringify(this.props)}</p>
+            </div>
+        );
+    }
+}
+ 
+export default Board;
